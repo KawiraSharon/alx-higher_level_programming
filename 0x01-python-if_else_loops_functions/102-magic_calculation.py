9f268e2398fc22cea61eb4e425d9fa4b8ab7a9f8
KawiraSharon <sharonkawira389@gmail.com>
2024-01-03 02:31:40 +0300

    morepython

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this code matches bytecode provided by Holberton School, isofoldrpyvrsn"""
+def magic_calculation(a, b, c):
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
