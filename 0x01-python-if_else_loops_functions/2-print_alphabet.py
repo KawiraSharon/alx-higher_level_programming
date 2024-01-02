94f63d8a4d2a1203f7146bef9ca3487302728351
KawiraSharon <sharonkawira389@gmail.com>
2024-01-03 01:31:09 +0300

    morepython

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for ASCII in range (97, 123):
-    print(chr(ASCII), end="")
+    print("{}".format(chr(ASCII)), end="")
+    # chr is used to convert int ascii to corresponding string value
