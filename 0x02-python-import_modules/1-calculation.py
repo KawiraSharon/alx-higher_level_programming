289cdeaec92e7279e0dd6ecc919fe30bb3e45f9e
KawiraSharon <sharonkawira389@gmail.com>
2024-01-04 19:57:45 +0300

    format-string is no f-string

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,12 +5,7 @@ import calculator_1 as calc
 a = 10
 b = 5
 
-summation = calc.add(a, b)
-difference = calc.sub(a, b)
-product = calc.mul(a, b)
-divisor = calc.div(a, b)
-
-print(f"{a} + {b} = {summation}")
-print(f"{a} - {b} = {difference}")
-print(f"{a} * {b} = {product}")
-print(f"{a} / {b} = {divisor}")
+print("{} + {} = {}".format(a, b, calc.add(a, b)))
+print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+print("{} / {} = {}".format(a, b, calc.div(a, b)))
