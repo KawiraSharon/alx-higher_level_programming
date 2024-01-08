5033ff6a482309d0fc6bd3f26b8bc549eeaf6db5
KawiraSharon <sharonkawira389@gmail.com>
2024-01-09 02:14:31 +0300

    task5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    newstring = " "
+    for idx in range(len(my_string)):
+        if (my_string[idx] != 'c') and (my_string[idx] != 'C'):
+            newstring += my_string[idx]
+    return newstring
