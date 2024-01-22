3d4826ff85ae9e9d0dce98f839ec012af4e9aa88
KawiraSharon <sharonkawira389@gmail.com>
2024-01-23 01:50:39 +0300

    Task1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
+    """Print integer with format string"""
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        return False
