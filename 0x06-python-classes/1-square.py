f2a98d46e34aeedd89aa3573b4d77bf6caf74997
KawiraSharon <sharonkawira389@gmail.com>
2024-01-24 03:52:27 +0300

    Task1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+"""Write a class Square that defines a square by Task0"""
+
+
+class Square:
+    """Square class syntatically defined"""
+
+    def __init__(self, size):
+        """
+        Square Initialization.
+        Args:
+            size: size of square
+        """
+        self.__size = size
