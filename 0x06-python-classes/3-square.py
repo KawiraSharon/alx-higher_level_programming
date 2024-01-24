df0ba77399332b8091eb19195db9f76d7bbb9d8b
KawiraSharon <sharonkawira389@gmail.com>
2024-01-24 04:25:42 +0300

    Task3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+"""Definition of class named square"""
+
+
+class Square:
+    """Square is denoted here"""
+
+    def __init__(self, size=0):
+        """Square is Initialized.
+        Args:
+            size: square size int
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Square size returned"""
+        return (self.__size * self.__size)
