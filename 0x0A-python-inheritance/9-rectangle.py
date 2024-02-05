040984d8b2008d9e0d0196f41d3f8fe05d5dece9
= <=>
2024-02-06 02:07:36 +0300

    Task  9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Class Rectangle inheriting from 7-base-geometry"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Reactangle class defined here"""
+
+    def __init__(self, width, height):
+        """args: self, width, height"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Area attribute defined"""
+        return self.__width* self.__height
+
+    def str(self):
+        """Function performs operator overloading"""
+        return "[Rectangle] " + str(self.__width) + '/' + str(self.__height)
