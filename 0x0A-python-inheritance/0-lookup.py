848f44715fd037303436a43177c182c69db6aba6
= <=>
2024-02-05 23:52:00 +0300

    Task0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Function return list of attributes and mthds of object"""
 
+
 def lookup(obj):
     """List attrbts of objct and return obj list"""
-    return(dir(obj))
+    return (dir(obj))
