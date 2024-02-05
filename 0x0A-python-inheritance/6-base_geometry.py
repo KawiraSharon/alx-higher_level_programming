42544dd9319692c6d451259b9a19b3ccedcebbb7
= <=>
2024-02-06 00:31:47 +0300

    Task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Class Basegeometry"""
+
+class BaseGeometry:
+    """Class BaseGeometry taking method"""
+    def area(self):
+        """Raise exception not named"""
+        raise Exception("area() is not implemented")
