6a84de97d87a66777ac32b63e9024c261cfbcf9d
= <=>
2024-02-06 00:46:41 +0300

    Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class Basegeometry"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry taking methods"""
+    def area(self):
+        """Raise exception not named"""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """
+        Methods are functions defined inside class
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
