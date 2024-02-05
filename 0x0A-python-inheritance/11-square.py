be33643dfe9b8ad9b1782c4f330b85b0d43b7c2f
= <=>
2024-02-06 02:40:55 +0300

    Task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -5,6 +5,9 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """Mthds/Attrbts of new square"""
-    self.integer_validator("size", size)
-    super().__init__(size, size)
-    self.__size = size
+
+    def __init___(self, size):
+        """args size"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
