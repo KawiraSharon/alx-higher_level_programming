eaec0b7e9e15f06e1a406f3f941eaaa0557c7cc8
= <=>
2024-02-06 01:46:18 +0300

    Task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,6 +2,7 @@
 """Class Rectangle inheriting from 7-base-geometry"""
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Reactangle class defined here"""
 
