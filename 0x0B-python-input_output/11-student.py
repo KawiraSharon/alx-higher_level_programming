7e4cb5070d5fa4314343f69bfb700b50ce1b507e
= <=>
2024-02-06 22:19:05 +0300

    Task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Definition of Class Student"""
+
+
+class Student:
+    """Student reprsnttn"""
+
+    def __init__(self, first_name, last_name, age):
+        """New student being initialized"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Args:
+            attrs: attributes being represented
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Args:
+            json: key pairs replacing attributes(dictionary)
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
