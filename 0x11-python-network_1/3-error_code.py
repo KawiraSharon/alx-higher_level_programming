a33bd1ddb700881c70cdc3daacef8774ab9ada74
= <=>
2024-04-27 01:35:20 +0300

    Task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+this piece of code sends request to URL, then displays body of response.
+"""
+import urllib.request
+from sys import argv
+
+
+def main(argv):
+    """
+    This method manages urllib.error.HTTPError exceptions
+    It also prints: Error code: follows it with its-HTTP status code
+    """
+    url = argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            result = response.read()
+            print(result.decode('utf8'))
+    except urllib.error.URLError as e:
+        print("Error code: {}".format(e.code))
+
+
+if __name__ == "__main__":
+    main(argv)
