a893cc28321e2909cad05cc27474d0689ee2eb41
= <=>
2024-04-27 01:57:09 +0300

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
-"""
-Script to fetch https://intranet.hbtn.io/status
-"""
+"""Script fetches this; https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
 
-def main():
-    """
-    Function that prints response of url that is specific.
-    """
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode('utf8')))
-
 if __name__ == "__main__":
-    main()
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
